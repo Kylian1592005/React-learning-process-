bbf81013eb593a3495c9028e00cf7d975b26d8f4
Kylian <ygnnyogyi79980>
2026-02-26 21:24:59 +0630

    Create Search button work

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -3,17 +3,21 @@ import { ProdcutsGrid } from './ProductsGrid'
 import { Header } from '../../components/header'
 import './HomePage.css'
 import axios from 'axios'
+import { useSearchParams } from 'react-router'
  
 export function HomePage( {cart, loadCart} ) {
   const[products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products');
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
     getHomeData();
-  }, []);
+  }, [search]);
 
   return (
      <>
